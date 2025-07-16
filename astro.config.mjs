0b73bb7d53fa06da993ada295d4b46c5d2c480f5
Andy Wilkinson <andrew@meonvalleyweb.com>
2025-07-16 17:44:39 +0100

    Updated netlify/functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'server',
   adapter: netlify(),
+  integrations: [tailwind()],
 });
